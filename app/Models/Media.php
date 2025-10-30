1c2b8456e5d89f174505325c614e9f379b558696
jameskl237 <djielejames@gmail.com>
2025-10-30 02:39:04 +0100

    last changes

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -2,12 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Media extends Model
 {
     protected $table = 'medias'; 
+     protected $appends = ['url'];
+
+    public function getUrlAttribute()
+    {
+        // Si le fichier existe, retourne une URL complète
+        return $this->file_path
+            ? Storage::url($this->file_path)
+            : null;
+    }
     
     protected $fillable = [
         'url',
